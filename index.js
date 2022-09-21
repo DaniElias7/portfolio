25dcdc7e542ed01301c940b6eaae4f47852ddfbf
oscar <or651059@gmail.com>
2022-09-20 23:07:50 -0300

    Setting some responsive matters

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const icon = document.querySelector( '#go-up-button' );
 const goUpIcon = document.querySelector( '#go-up-icon' );
+const menuIcon = document.querySelector( '#menu' );
+
+menuIcon.addEventListener('click', () => {
+    const ul = document.querySelector("nav>ul");
+    let hideMenu = ul.classList.contains('show-menu');
+
+    hideMenu ? ul.classList.remove('show-menu') : ul.classList.add('show-menu');
+});
+
 
 let footerYear = document.querySelector('#footer-year');
 footerYear.innerHTML = new Date( ).getFullYear( );
