708acd9f14601bde1071ea737a36644442e67f8d
oscar <or651059@gmail.com>
2022-09-25 17:43:01 -0300

    creating toggleMenu function

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const icon = document.querySelector( '#go-up-button' );
 const goUpIcon = document.querySelector( '#go-up-icon' );
 const menuIcon = document.querySelector( '#menu' );
 
-menuIcon.addEventListener('click', () => {
+const menuHandle = () => {
     const ul = document.querySelector("nav>ul");
     let hideMenu = ul.classList.contains('show-menu');
 
     hideMenu ? ul.classList.remove('show-menu') : ul.classList.add('show-menu');
-});
+}
+menuIcon.addEventListener('click', () => menuHandle() );
 
+const anchors = document.querySelectorAll("nav>ul>li>a");
+for (let a of anchors) {
+    a.addEventListener('click', () => menuHandle() );
+}
 
 let footerYear = document.querySelector('#footer-year');
 footerYear.innerHTML = new Date( ).getFullYear( );
